f87df35a3b4ed3cf06b7c1277686378263206dad
Mike Chagnon <mike@kaimika.com>
2022-05-19 17:28:41 -0400

    add initial integration with external MICADAS database

---
+++ SNICSr/Models/MicadasRecord.vb
@@ -0,0 +1,17 @@
+﻿Public Class MicadasRecord
+    Public IsOk As Boolean
+    Public RunTime As DateTime
+    Public Position As Integer
+    Public Measurement As Integer
+    Public SampleName As String
+    Public SampleType As String = ""
+    Public Cycles As Integer
+    Public LE12C As Double
+    Public HE12C As Double
+    Public HE13C As Double
+    Public CntTotH As Integer
+    Public CntTotS As Integer
+    Public CntTotGT As Integer
+    Public HE13Over12 As Double
+    Public HE14Over12 As Double
+End Class
